765cfe980aeadfb75079286464b57805a732bad5
reyvenRJS <reyven.rojas@gmail.com>
2024-05-03 23:00:50 +0800

    Determine active nav button

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,29 @@
 import { Link } from 'react-router-dom';
+import '../Navbar/Navbar.css';
 
 const Navbar = () => {
+    const OnActiveButton = (event) => {
+        event.target.classList.add('active-nav-btn');
+      
+        const siblings = Array.from(event.target.parentNode.children);
+        siblings.forEach((sibling) => {
+          if (sibling !== event.target) {
+            sibling.classList.remove('active-nav-btn');
+          }
+        });
+
+      };
+
     return (
         <div className="div-links-container">
-            <Link to="/" className='icon'><div className="div-myIcon"></div></Link>
+            <Link onClick={() => window.location.href="/"} to="/" className='icon'><div className="div-myIcon"></div></Link>
             <div className="div-links-buttons">
-                <Link to="/">Home</Link>
-                <Link to="/AboutMe">About</Link>
-                <Link to="/Skill">Skills</Link>
-                <Link to="/Works">Works</Link>
-                <Link to="/Projects">Projects</Link>
-                <Link to="/Contact">Contact</Link>
+                <Link className="active-nav-btn" onClick={OnActiveButton} to="/">Home</Link>
+                <Link onClick={OnActiveButton} to="/AboutMe">About</Link>
+                <Link onClick={OnActiveButton} to="/Skill">Skills</Link>
+                <Link onClick={OnActiveButton} to="/Works">Works</Link>
+                <Link onClick={OnActiveButton} to="/Projects">Projects</Link>
+                <Link onClick={OnActiveButton} to="/Contact">Contact</Link>
             </div>
         </div>
     );
